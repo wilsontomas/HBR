09aae00925c9c3d64486cb4e23a054c2bd9688f2
Wilson Tomas <wilsonpumm@gmail.com>
2020-08-22 12:38:14 -0400

    ejercicios del 1 y 2

---
+++ EjerciciosDeConsola/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            bool exit = true;
+            int number = 0;
+            int result, resto;
+            do {
+                Console.WriteLine("Ingrese el primer numero");
+               var x = Console.ReadLine();
+                if (!int.TryParse(x, out number)) { Console.WriteLine("No es un numero"); continue; }
+                Console.WriteLine("Ingrese el segundo numero");
+               var y = Console.ReadLine();
+                if (!int.TryParse(y, out number)) { Console.WriteLine("No es un numero"); continue; }
+
+                
+                result = int.Parse(x) / int.Parse(y);
+                resto = int.Parse(x) % int.Parse(y);
+                Console.WriteLine($"La divicion es {result} y el resto es {resto}");
+                break;
+
+
+            } while (exit);
+           
+           
+        }
+    }
+}
---
+++ EjerciciosDeConsola/Ejercicio1/Ejercicio2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            bool exit = true;
+            int number = 0;
+            int resto;
+            do
+            {
+                Console.WriteLine("Saber si el primero es multiplo del segundo");
+                Console.WriteLine("Ingrese el primer numero");
+                var x = Console.ReadLine();
+                if (!int.TryParse(x, out number)) { Console.WriteLine("No es un numero"); continue; }
+                Console.WriteLine("Ingrese el segundo numero");
+                var y = Console.ReadLine();
+                if (!int.TryParse(y, out number)) { Console.WriteLine("No es un numero"); continue; }
+      
+                resto = int.Parse(x) % int.Parse(y);
+                if(resto == 0) { Console.WriteLine($"{x} es multiplo de {y}"); } else { Console.WriteLine("No es multiplo"); }
+                
+                break;
+
+
+            } while (exit);
+
+        }
+    }
+}
